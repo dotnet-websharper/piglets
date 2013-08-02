1f7dbef3f2bc5d6a4b3a8976a82c8de1072627d6
Loic Denuziere <loic.denuziere@intellifactory.com>
2013-08-02 10:30:09 +0200

    Change the sample to better take advantage of type inference.

--- IntelliFactory.WebSharper.Piglets.Test/App.fs
+++ IntelliFactory.WebSharper.Piglets.Test/App.fs
@@ -67,93 +67,92 @@ module View =
             if x.isSuccess then "white" else "#ffa0a0")
 
     [<JavaScript>]
-    let User (firstName, lastName) age gender comments participates (friends: Many.Renderer<_,_,_>) liveUser submit =
-        let nameInput s =
-            C.Input s |> RedBgOnError (s.Through liveUser)
-        Div [
-            Div [nameInput firstName |> C.WithLabel "First name:"]
-            Div [nameInput lastName |> C.WithLabel "Last name:"]
-            Div [C.Radio gender [Male, "Male"; Female, "Female"]]
-            Div [C.IntInput age |> RedBgOnError (age.Through liveUser) |> C.WithLabel "Age:"]
-            Div [C.CheckBox participates |> C.WithLabel "Participate in the survey"]
-            Div [C.TextArea comments |> C.WithLabel "Comments:"]
-            Div [] |> C.RenderMany friends (fun ops (first, last) ->
-                Div [
-                    nameInput first
-                    nameInput last
-                    C.Button ops.Delete -< [B [Text "Delete this friend"]]
-                    C.ButtonValidate ops.MoveUp -< [Text "Move up"]
-                    C.ButtonValidate ops.MoveDown -< [Text "Move down"]
-                ])
-                |> C.WithLabel "Friends:"
-            Div [C.Button friends.Add -< [B [Text "Add a friend"]]]
-            Table [
-                TBody [
-                    TR [
-                        TH [Attr.ColSpan "7"] -< [Text "Summary"]
-                    ]
-                    TR [
-                        TH [Text "First name"]
-                        TH [Text "Last name"]
-                        TH [Text "Gender"]
-                        TH [Text "Age"]
-                        TH [Text "Participates"]
-                        TH [Text "Comments"]
-                        TH [Text "Friends"]
-                    ]
-                    TR [
-                        // These will only show up if the whole user is valid
-                        TD [] |> C.ShowString liveUser (fun u -> u.name.firstName)
-                        TD [] |> C.ShowString liveUser (fun u -> u.name.lastName)
-                        TD [] |> C.ShowString liveUser (fun u -> if u.gender = Male then "Male" else "Female")
-                        TD [] |> C.ShowString liveUser (fun u -> string u.age)
-                        // This one will show up even if other parts are invalid
-                        // because it uses the `participates` stream instead of `liveUser`
-                        TD [] |> C.ShowString participates (function
-                                | true -> "Yes"
-                                | false -> "No")
-                            |> C.Css participates "font-weight" (function
-                                | true -> "bold"
-                                | false -> "normal")
-                        TD [] |> C.Show liveUser (function
-                            | {comments = ""} -> [I [Text "(no comment)"]]
-                            | {comments = c} -> [Span [Text c]])
-                        TD [] |> C.ShowString friends.Output
-                            (Seq.map (fun n -> n.firstName + " " + n.lastName)
-                            >> String.concat ", ")
+    let User init =
+        ViewModel.User init
+        |> Piglet.Render (fun (firstName, lastName) age gender comments participates friends liveUser submit ->
+            let nameInput s =
+                C.Input s |> RedBgOnError (s.Through liveUser)
+            Div [
+                Div [nameInput firstName |> C.WithLabel "First name:"]
+                Div [nameInput lastName |> C.WithLabel "Last name:"]
+                Div [C.Radio gender [Male, "Male"; Female, "Female"]]
+                Div [C.IntInput age |> RedBgOnError (age.Through liveUser) |> C.WithLabel "Age:"]
+                Div [C.CheckBox participates |> C.WithLabel "Participate in the survey"]
+                Div [C.TextArea comments |> C.WithLabel "Comments:"]
+                Div [] |> C.RenderMany friends (fun ops (first, last) ->
+                    Div [
+                        nameInput first
+                        nameInput last
+                        C.Button ops.Delete -< [B [Text "Delete this friend"]]
+                        C.ButtonValidate ops.MoveUp -< [Text "Move up"]
+                        C.ButtonValidate ops.MoveDown -< [Text "Move down"]
+                    ])
+                    |> C.WithLabel "Friends:"
+                Div [C.Button friends.Add -< [B [Text "Add a friend"]]]
+                Table [
+                    TBody [
+                        TR [
+                            TH [Attr.ColSpan "7"] -< [Text "Summary"]
+                        ]
+                        TR [
+                            TH [Text "First name"]
+                            TH [Text "Last name"]
+                            TH [Text "Gender"]
+                            TH [Text "Age"]
+                            TH [Text "Participates"]
+                            TH [Text "Comments"]
+                            TH [Text "Friends"]
+                        ]
+                        TR [
+                            // These will only show up if the whole user is valid
+                            TD [] |> C.ShowString liveUser (fun u -> u.name.firstName)
+                            TD [] |> C.ShowString liveUser (fun u -> u.name.lastName)
+                            TD [] |> C.ShowString liveUser (fun u -> if u.gender = Male then "Male" else "Female")
+                            TD [] |> C.ShowString liveUser (fun u -> string u.age)
+                            // This one will show up even if other parts are invalid
+                            // because it uses the `participates` stream instead of `liveUser`
+                            TD [] |> C.ShowString participates (function
+                                    | true -> "Yes"
+                                    | false -> "No")
+                                |> C.Css participates "font-weight" (function
+                                    | true -> "bold"
+                                    | false -> "normal")
+                            TD [] |> C.Show liveUser (function
+                                | {comments = ""} -> [I [Text "(no comment)"]]
+                                | {comments = c} -> [Span [Text c]])
+                            TD [] |> C.ShowString friends.Output
+                                (Seq.map (fun n -> n.firstName + " " + n.lastName)
+                                >> String.concat ", ")
+                        ]
                     ]
                 ]
-            ]
-            Div [] |> C.ShowErrors liveUser (fun msgs ->
-                [
-                    Div [Attr.Style "border:solid 1px #c00;color:#c00;margin:10px;padding:5px"] -< [
-                        for m in msgs do
-                            yield Text m
-                            yield (Br [] :> _)
-                    ]
-                ])
-            Div [C.Submit submit]
-            Div [
-                Br []; Br []
-                C.IntInput age |> C.WithLabel "Age again:"
-                Span [Text "(just to test several inputs connected to the same stream)"]
-            ]
-        ]
+                Div [] |> C.ShowErrors liveUser (fun msgs ->
+                    [
+                        Div [Attr.Style "border:solid 1px #c00;color:#c00;margin:10px;padding:5px"] -< [
+                            for m in msgs do
+                                yield Span [Text m]
+                                yield Br []
+                        ]
+                    ])
+                Div [C.Submit submit]
+                Div [
+                    Br []; Br []
+                    C.IntInput age |> C.WithLabel "Age again:"
+                    Span [Text "(just to test several inputs connected to the same stream)"]
+                ]
+            ])
 
 [<JavaScript>]
 let UI() =
-    let initUser : Model.User =
-        {
-            name = { firstName = "John"; lastName = "Rambo" }
-            age = 40
-            gender = Model.Male
-            comments = "Blah blah blah"
-            participates = true
-            friends =
-                [|
-                    { firstName = "Ernesto"; lastName = "Rodriguez" }
-                    { firstName = "Loic"; lastName = "Denuziere" }
-                |]
-        }
-    ViewModel.User initUser
-    |> Piglet.Render View.User
+    View.User {
+        name = { firstName = "John"; lastName = "Rambo" }
+        age = 40
+        gender = Model.Male
+        comments = "Blah blah blah"
+        participates = true
+        friends =
+            [|
+                { firstName = "Ernesto"; lastName = "Rodriguez" }
+                { firstName = "Loic"; lastName = "Denuziere" }
+            |]
+    }
