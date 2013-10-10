99ab98bc872df482e2cbbab04e6353ec1cfc6ade
"Anton Tayanovskyy (t0yv0) ext:(%22) <anton.tayanovskyy@gmail.com>
2013-10-10 11:47:43 -0400

    Build updatse.

--- build.fsx
+++ build.fsx
@@ -3,7 +3,11 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("IntelliFactory.WebSharper.Piglets", "0.1")
+    BuildTool().PackageId("WebSharper.Piglets", "0.1-alpha")
+        .References(fun r ->
+            [
+                r.Assembly("System.Web")
+            ])
 
 let main =
     bt.WebSharper.Library("IntelliFactory.WebSharper.Piglets")
@@ -16,14 +20,45 @@ let main =
                     r.NuGet("WebSharper").Reference()
                 ])
 
+let test =
+    bt.WebSharper.Library("IntelliFactory.WebSharper.Piglets.Test")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.Project(main)
+            ])
+
+let web =
+    bt.WebSharper.HostWebsite("Web")
+        .References(fun r ->
+            let path =
+                [
+                    "tools/net45/IntelliFactory.Xml.dll"
+                ]
+            [
+                r.Project(main)
+                r.Project(test)
+                r.NuGet("WebSharper").At(path).Reference() // Looks like if.build bug?
+            ])
+
 bt.Solution [
 
     main
+    test
+    web
 
     bt.NuGet.CreatePackage()
         .Description("Provides a framework to build reactive interfaces in WebSharper,
             similar to Formlets but with more control over the structure of the output.")
-        .ProjectUrl("http://bitbucket.com/IntelliFactory/websharper.piglets")
+        .ProjectUrl("http://github.com/intellifactory/websharper.piglets")
+        .Configure(fun c ->
+            {
+                c with
+                    Authors = ["IntelliFactory"]
+                    Title = Some "WebSharper.Piglets"
+                    LicenseUrl = Some "http://github.com/intellifactory/websharper.piglets/blob/master/LICENSE.md"
+                    RequiresLicenseAcceptance = true
+            })
         .Add(main)
 
 ]
