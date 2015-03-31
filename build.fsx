7633154f8b9f8cef5b5a9de4024e2f28283efadb
András Jankó <andras.janko@gmail.com>
2015-03-31 19:30:44 +0200

    remove alpha tag

--- build.fsx
+++ build.fsx
@@ -3,7 +3,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.Piglets", "3.0-alpha")
+    BuildTool().PackageId("WebSharper.Piglets", "3.0")
         .References(fun r ->
             [
                 r.Assembly("System.Web")
