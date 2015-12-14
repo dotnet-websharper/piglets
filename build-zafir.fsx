6a8de99c551eb5d1884f4e7000b5adca7f8208cb
István Gansperger <gansperger@gmail.com>
2015-12-14 12:31:27 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,50 @@
+#load "tools/includes.fsx"
+
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.Piglets")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let main =
+    bt.Zafir.Library("WebSharper.Piglets")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.Reactive").ForceFoundVersion().Reference()
+                r.NuGet("Zafir.Html").ForceFoundVersion().Reference()
+            ])
+
+let test =
+    bt.Zafir.HtmlWebsite("WebSharper.Piglets.Test")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.Reactive").Reference()
+                r.NuGet("Zafir.Html").Reference()
+                r.Project(main)
+            ])
+
+bt.Solution [
+
+    main
+    test
+
+    bt.NuGet.CreatePackage()
+        .Description("Provides a framework to build reactive interfaces in Zafir,
+            similar to Formlets but with more control over the structure of the output.")
+        .ProjectUrl("http://github.com/intellifactory/websharper.piglets")
+        .Configure(fun c ->
+            {
+                c with
+                    Authors = ["IntelliFactory"]
+                    Title = Some "Zafir.Piglets"
+                    LicenseUrl = Some "http://github.com/intellifactory/websharper.piglets/blob/master/LICENSE.md"
+                    RequiresLicenseAcceptance = true
+            })
+        .Add(main)
+
+]
+|> bt.Dispatch
